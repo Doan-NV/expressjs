abfdbe642577efbaad9d06e05e7ba7f04117c799
Doanne <yileuu37na@gmail.com>
2021-04-16 00:12:17 +0700

    thay doi dang nhạp su dung cookie

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -7,8 +7,9 @@ const db = require('../config/db/connection');
 // require admin model
 const adminModel = require('../models/admin.models');
 
-// [GET login]
-module.exports.login = (req, res) => {
+module.exports.index
+    // [GET login]
+module.exports.getLogin = (req, res) => {
     res.render('admin/login');
 };
 // [POST login]
@@ -16,7 +17,7 @@ module.exports.postLogin = async(req, res) => {
     console.log('chay vao day')
     let username = req.body.username;
     let password = req.body.password;
-    console.log(username, password)
+    // console.log(username, password)
     let errors = [];
     let checked = await adminModel.checkingAdmin(username, password);
 
@@ -29,10 +30,19 @@ module.exports.postLogin = async(req, res) => {
         errors.push('user name or password is wrong!!')
         res.render('admin/login', { errors })
     } else {
-        res.render('admin/dashboard')
+        // set cookie
+        res.cookie('username', username);
+
+        res.redirect('dashboard');
     }
     // console.log(checked);
-    // console.log('hahaaa')
+    console.log(req.cookies.username)
+        // console.log('hahaaa')
 
 
 };
+
+// [GET] dashboard
+module.exports.dashboard = (req, res) => {
+    res.render('admin/dashboard');
+}
