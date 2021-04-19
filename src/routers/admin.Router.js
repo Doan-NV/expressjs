06cf57574293b01f03bb027fe4bb2e163a0ef11d
Doanne <yileuu37na@gmail.com>
2021-04-19 22:08:28 +0700

    update trang login băng cookie

--- src/routers/admin.Router.js
+++ src/routers/admin.Router.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const adminController = require('../controllers/admin.controllers')
-const requireAuth = require('../middlewares/auth.middleware')
+const adminController = require('../controllers/admin.controllers');
+const requireAuth = require('../middlewares/auth.middleware');
 router.get('/login', requireAuth.requireAuth, adminController.getLogin);
-router.post('/login', adminController.postLogin)
+router.post('/login', adminController.postLogin);
 
 router.get('/dashboard', requireAuth.requireAuth, adminController.dashboard);
 
